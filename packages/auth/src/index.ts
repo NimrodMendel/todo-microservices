e79979538c6a71756828dd6da50ac33d535d7d12
Nimrod Mendel <mendel.nimrod@gmail.com>
2025-02-12 19:53:03 +0200

    first commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(3010, async () => {
+  console.log(`server ready to accept requests on port ${3010}`);
+});
