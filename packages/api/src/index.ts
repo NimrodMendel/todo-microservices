84f1385e65c66860b0ee0851f13d32f5eb745058
Nimrod Mendel <mendel.nimrod@gmail.com>
2025-03-02 21:05:15 +0200

    Add cors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -28,7 +28,12 @@ const speedLimiter = slowDown({
 
 app.use(limiter);
 app.use(speedLimiter);
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://dashboard:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
